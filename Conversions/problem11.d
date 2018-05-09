746e472e57f683687572bef24ba200c7fb33e40d
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-10 07:41:31 +0800

    2018 Updates

---
+++ Conversions/problem11.d
@@ -0,0 +1,11 @@
+//Problem 11: Write a program that converts numbers of day to minutes.
+import std.stdio;
+
+int main( ){
+ int day = 1;
+ int minutes = day * 24 * 60;
+ 
+ writeln(minutes);
+  
+ return 0;
+}
