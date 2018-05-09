fc1acd29f6e3a2e87af2a3952d924af79aaeea1b
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-10 07:42:47 +0800

    2018 Updates

---
+++ Conversions/problem15.d
@@ -0,0 +1,13 @@
+//Problem 15: Write a program that converts letters to ASCII.
+import std.stdio;
+import std.string;
+import std.conv;
+import std.ascii;
+
+int main(){
+ char letter = 'x';
+ 
+ writeln(to!int(letter));
+  
+ return 0;
+}
