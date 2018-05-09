7741d679c6d55925ba53f48d08f1a8cd2fbb9eff
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-10 07:42:03 +0800

    2018 Updates

---
+++ Conversions/problem12.d
@@ -0,0 +1,11 @@
+//Problem 12: Write a program that converts numbers of day to seconds.
+import std.stdio;
+
+int main( ){
+ int day = 1;
+ int seconds = day * 24 * 60 * 60;
+ 
+ writeln(seconds);
+  
+ return 0;
+}
