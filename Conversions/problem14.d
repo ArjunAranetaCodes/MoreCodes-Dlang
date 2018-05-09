d6be7c4ff7987191b805b6cfab2d6a0cafb001fb
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-10 07:42:32 +0800

    2018 Updates

---
+++ Conversions/problem14.d
@@ -0,0 +1,13 @@
+//Problem 14: Write a program that converts lower case to upper case characters.
+import std.stdio;
+import std.string;
+import std.conv;
+import std.ascii;
+
+int main(){
+ string str = toUpper("abc");
+ 
+ writeln(str);
+  
+ return 0;
+}
