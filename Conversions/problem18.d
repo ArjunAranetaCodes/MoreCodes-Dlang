f2c1d4cd515887134b9cb380d48c447e30cf2b48
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-12 09:47:02 +0800

    2018 Updates

---
+++ Conversions/problem18.d
@@ -0,0 +1,11 @@
+//Problem 18: Write a program that converts liter to milliliter.
+import std.stdio;
+
+int main( ){
+ int liter = 1;
+ int millimeter = liter * 1000;
+ 
+ writeln(millimeter);
+  
+ return 0;
+}
