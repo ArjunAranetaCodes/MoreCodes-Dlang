0f5102e584dee074e87a50e156401f2c3f7d8e28
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-10-16 05:00:00 +0100

    2018 Updates

---
+++ Number Patters/problem4.d
@@ -0,0 +1,25 @@
+/*
+Problem 4: Write a program to print the number pattern of ones and zeros using loop.
+10001
+10001
+10001
+10001
+10001
+*/
+
+import std.stdio;
+
+int main(){
+	for(int y = 0; y < 5; y++){
+		for(int x = 0; x < 5; x++){
+			if((x == 0) || (x == 4)){
+        write("1");	
+			}else{
+        write("0");	
+			}			
+		}
+    write("\n");	
+	}
+	
+	return 0;
+}
